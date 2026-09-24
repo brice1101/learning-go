fc82f2a606c8add1c407cac8119e5a056401c91a
BrandonLodge <brandon@brandonlodge.dev>
2026-09-24 13:16:45 +0100

    Refactor vector implementation and expand test coverage.

--- datastructures/vector/vector.go
+++ datastructures/vector/vector.go
@@ -1,22 +1,48 @@
 package vector
 
 type Vector struct {
-	data     []int
-	capacity int
-	length   int
+	data   []int
+	length int
+}
+
+func New() *Vector {
+	return &Vector{}
+}
+
+func NewWithCapacity(capacity int) *Vector {
+	if capacity < 0 {
+		return nil
+	}
+	return &Vector{
+		data: make([]int, capacity),
+	}
+}
+
+func (v *Vector) Len() int {
+	return v.length
+}
+
+func (v *Vector) Cap() int {
+	return len(v.data)
+}
+
+func (v *Vector) Get(i int) (int, bool) {
+	if i >= v.length || i < 0 {
+		return 0, false
+	}
+	return v.data[i], true
+}
+
+func (v *Vector) Set(i int, value int) bool {
+	if i >= v.length || i < 0 {
+		return false
+	}
+	v.data[i] = value
+	return true
 }
 
 func (v *Vector) Push(value int) {
-	if v.capacity == v.length {
-		if v.capacity == 0 {
-			v.capacity = 1
-		} else {
-			v.capacity *= 2
-		}
-		empty := make([]int, v.capacity)
-		copy(empty, v.data)
-		v.data = empty
-	}
+	v.growIfFull(v.length)
 	v.data[v.length] = value
 	v.length++
 }
@@ -31,6 +57,41 @@ func (v *Vector) Pop() (int, bool) {
 	return value, true
 }
 
-func (v *Vector) Get(index int) int {
-	return v.data[index]
+func (v *Vector) Insert(i int, value int) bool {
+	if i > v.length || i < 0 {
+		return false
+	}
+	v.growIfFull(i)
+	v.data[i] = value
+	v.length++
+	return true
+}
+
+func (v *Vector) Remove(i int) (int, bool) {
+	if i >= v.length || i < 0 {
+		return 0, false
+	}
+	value := v.data[i]
+	for j := i + 1; j < v.length; j++ {
+		v.data[j-1] = v.data[j]
+	}
+	v.data[v.length-1] = 0
+	v.length--
+	return value, true
+}
+
+func (v *Vector) growIfFull(gap int) {
+	if capacity := len(v.data); v.length == capacity {
+		if capacity == 0 {
+			capacity = 1
+		} else {
+			capacity *= 2
+		}
+		empty := make([]int, capacity)
+		copy(empty[:gap], v.data[:gap])
+		copy(empty[gap+1:], v.data[gap:])
+		v.data = empty
+	} else {
+		copy(v.data[gap+1:], v.data[gap:])
+	}
 }
--- datastructures/vector/vector_test.go
+++ datastructures/vector/vector_test.go
@@ -1,50 +1,138 @@
 package vector
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestNewVectorHasZeroLength(t *testing.T) {
-
-	// Test new vector has length of 0
-	newVector := Vector{}
-	if newVector.length != 0 {
-		t.Errorf("Expected length of newVector to be 0, but got %d", newVector.length)
+	vector := New()
+	if vector.length != 0 { // If new vector is not zero length
+		t.Errorf("Expected length of newVector to be 0, but got %d", vector.length)
 	}
 }
 
-func TestPushGrowsCapacity(t *testing.T) {
-	// Test vectors increase capacity when necessary
-	pushVector := Vector{}
-	pushVector.Push(1)
-	pushVector.Push(2)
-	pushVector.Push(3)
-	if pushVector.Get(0) != 1 {
-		t.Errorf("Expected pushVector.Get(0) to be 1, but got %s", fmt.Sprint(pushVector))
+func TestPush(t *testing.T) {
+	vector := New()
+	vector.Push(1)
+	if vector.length != 1 {
+		t.Errorf("Expected length of vector to be 1, but got %d", vector.length)
 	}
-	if pushVector.Get(1) != 2 {
-		t.Errorf("Expected pushVector.Get(1) to be 2, but got %s", fmt.Sprint(pushVector))
+	if value, _ := vector.Get(0); value != 1 {
+		t.Errorf("Expected vector.Get(0) to be 1, but got %d", value)
 	}
-	if pushVector.Get(2) != 3 {
-		t.Errorf("Expected pushVector.Get(2) to be 3, but got %s", fmt.Sprint(pushVector))
+}
+
+func TestCapacity(t *testing.T) {
+	vector := NewWithCapacity(2)
+	vector.Push(1)
+	vector.Push(2)
+	vector.Push(3)
+	if value, _ := vector.Get(0); value != 1 { // Test capacity increase conserves order
+		t.Errorf("Expected vector.Get(0) to be 1, but got %v", value)
 	}
-	if pushVector.capacity != 4 { // doubling growth policy
-		t.Errorf("Expected capacity of pushVector to be 4, but got %d", pushVector.capacity)
+	if value, _ := vector.Get(1); value != 2 {
+		t.Errorf("Expected vector.Get(1) to be 2, but got %v", value)
+	}
+	if vector.Cap() != 4 { // Check capacity doubles after push
+		t.Errorf("Expected capacity of vector to be 4, but got %d", vector.Cap())
 	}
 }
 
 func TestPop(t *testing.T) {
-	popVector := Vector{}
-	popVector.Push(1)
-	popVector.Pop()
-	if popVector.length != 0 {
-		t.Errorf("Expected popVector.length to be 0, but got %d", popVector.length)
+	vector := New()
+	vector.Push(1)
+	if value, _ := vector.Pop(); value != 1 {
+		t.Errorf("Expected vector.Pop() to be 2, but got %d", value)
 	}
-	if popVector.capacity != 1 {
-		t.Errorf("Expected popVector.capacity to be 1, but got %d", popVector.capacity)
+	if vector.length != 0 {
+		t.Errorf("Expected length of vector to be 1 after pop, but got %d", vector.length)
 	}
-	if _, ok := popVector.Pop(); ok { // Test popVector.Pop() returns nil when vector is empty
-		t.Errorf("Expected popVector.Pop() to return false, but got %s", fmt.Sprint(ok))
+	if _, ok := vector.Pop(); ok {
+		t.Errorf("Expected vector.Pop() on empty vector to return pop, but got %t", ok)
+	}
+}
+
+func TestGet(t *testing.T) {
+	vector := New()
+	vector.Push(1)
+	if _, ok := vector.Get(-1); ok {
+		t.Errorf("Expected vector.Get(-1) on vector with length 1 to return false, but got %t", ok)
+	}
+	if _, ok := vector.Get(1); ok {
+		t.Errorf("Expected vector.Get(1) on vector with length 1 to return false, but got %t", ok)
+	}
+}
+
+func TestSet(t *testing.T) {
+	vector := New()
+	vector.Push(1)
+	vector.Push(2)
+	vector.Set(0, 3)
+	if value, _ := vector.Get(0); value != 3 {
+		t.Errorf("Expected vector.Get(0) to be 3, but got %d", value)
+	}
+	if vector.length != 2 {
+		t.Errorf("Expected length of vector to be 2 after set, but got %d", vector.length)
+	}
+	vector.Set(3, 4)
+	if vector.Set(3, 4) == true {
+		t.Errorf("Expected vector.Set(3, 4) on vector with length 2 to return false, but got %t", vector.Set(3, 4))
+	}
+}
+
+func TestInsert(t *testing.T) {
+	vector := New()
+	vector.Push(1)
+	vector.Push(2)
+	vector.Insert(1, 3)
+	if value, _ := vector.Get(1); value != 3 {
+		t.Errorf("Expected vector.Get(1) to be 3, but got %d", value)
+	}
+	if vector.length != 3 {
+		t.Errorf("Expected length of vector to be 3 after insert, but got %d", vector.length)
+	}
+	if value, _ := vector.Get(2); value != 2 {
+		t.Errorf("Expected vector.Get(2) to be 2, but got %d", value)
+	}
+}
+
+func TestInsertEdgeCases(t *testing.T) {
+	vector := New()
+	vector.Push(1)
+	vector.Insert(0, 2)
+	if value, _ := vector.Get(0); value != 2 { // Test insert to start
+		t.Errorf("Expected vector.Get(0) to be 1, but got %d", value)
+	}
+	vector.Insert(2, 3)
+	if value, _ := vector.Get(2); value != 3 { // Test insert to end
+		t.Errorf("Expected vector.Get(2) to be 3, but got %d", value)
+	}
+	if vector.length != 3 {
+		t.Errorf("Expected length of vector to be 3 after insert, but got %d", vector.length)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	vector := New()
+	vector.Push(1)
+	vector.Push(2)
+	vector.Push(3)
+	vector.Remove(1)
+	if value, _ := vector.Get(1); value != 3 {
+		t.Errorf("Expected vector.Get(1) to be 3, but got %d", value)
+	}
+	if vector.length != 2 {
+		t.Errorf("Expected length of vector to be 2 after remove, but got %d", vector.length)
+	}
+}
+
+func TestCapacityGrowth(t *testing.T) {
+	vector := New()
+	for i := range 1000 {
+		vector.Push(i)
+		vector.Pop()
+	}
+	if vector.Cap() != 1 {
+		t.Errorf("Expected capacity of vector to be 0 after 1000 pushes and pops, but got %d", vector.Cap())
 	}
 }
